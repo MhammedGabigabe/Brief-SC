5e395f3f4fee8bc498b6de35e0c31a629248e2ce
Gabigabe <mhammed.gabigabe@gmail.com>
2025-11-21 00:08:24 +0100

    feat(GP-32): implémentation d’un formulaire dynamique permettant d’ajouter plusieurs expériences

--- general.js
+++ general.js
@@ -17,6 +17,15 @@ const para_url = document.getElementById("para-url");
 const para_email = document.getElementById("para-email");
 const para_phone = document.getElementById("para-phone");
 const id_photo = document.getElementById("id-photo");
+const para_entreprise = document.getElementById("para-entreprise");
+const para_poste = document.getElementById("para-poste");
+const para_debut = document.getElementById("para-debut");
+const para_fin = document.getElementById("para-fin");
+
+const nom_entreprise = document.getElementById("nom-entreprise");
+
+const partie_experience = document.getElementById("partie-experience");
+const btn_ajouter_experience = document.getElementById("btn-ajouter-experience");
 
 
 
@@ -26,6 +35,8 @@ function verification_presence_format() {
     const format_url = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w\-._~:/?#\[\]@!$&'()+,;=])?$/;
     const format_telephone = /^(05|06|07)\d{8}$/;
     const format_email = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+
+    const format_entreprise = /^(?=.*[a-zA-ZÀ-ÿ])[a-zA-ZÀ-ÿ\s\d]+$/;
 
     if (nom_employe.value.trim() == "") {
         para_nom.classList.remove('hidden');
@@ -74,7 +85,52 @@ function verification_presence_format() {
         para_email.textContent = "*Format n'est pas valide !!"
     }
 
+    alert("apres de tester");
+    if(format_entreprise.test(nom_entreprise.value)){
+        alert("c bien");
+        para_entreprise.classList.add('hidden');
+    }else{
+        alert("c parfait");
+        para_entreprise.classList.remove('hidden');
+        para_entreprise.textContent = "*Format n'est pas valide !!";
+    }
+
+    alert("avant de tester");
 }
+
+function injecter_element(){
+    const element = document.createElement('div');
+    element.innerHTML =`
+        <div class="flex gap-1 mt-2 w-[100%]">
+          <div class="flex flex-col w-[45%] ml-4">
+            <label for="nom-entreprise" class="text-sm text-gray-800">Entreprise</label>
+            <input type="text" id="nom-entreprise" class="rounded-md border-2 border-gray p-1 text-sm">
+            <p id="para-entreprise" class="text-red-500 text-xs hidden"></p>
+          </div>
+          <div class="flex flex-col w-[45%] ml-4">
+            <label for="poste" class="text-sm text-gray-800">Poste</label>
+            <input type="text" id="poste" class="rounded-md border-2 border-gray p-1 text-sm">
+            <p id="para-poste" class="text-red-500 text-xs hidden"></p>
+          </div>
+        </div>
+        <div class="flex gap-1 mt-2 mb-4 w-[100%]" id="">
+          <div class="flex flex-col w-[45%] ml-4">
+            <label for="date-debut" class="text-sm text-gray-800">Date de début</label>
+            <input type="date" id="date-debut" class="rounded-md border-2 border-gray p-1 text-sm">
+            <p id="para-debut" class="text-red-500 text-xs hidden"></p>
+          </div>
+          <div class="flex flex-col w-[45%] ml-4">
+            <label for="date-fin" class="text-sm text-gray-800">Date de fin</label>
+            <input type="date" id="date-fin" class="rounded-md border-2 border-gray p-1 text-sm">
+            <p id="para-fin" class="text-red-500 text-xs hidden"></p>
+          </div>
+        </div>`;
+    partie_experience.append(element);
+}
+
+btn_ajouter_experience.addEventListener('click', ()=>{
+    injecter_element();
+})
 
 url_photo.addEventListener('input',()=>{
     
@@ -95,6 +151,16 @@ btn_ajouter.addEventListener('click', () => {
 })
 btn_quitter.addEventListener('click', () => {
     modale_ajout.classList.add('hidden');
+    nom_employe.value = "";
+    liste_role.value = "";
+    url_photo.value = "";
+    email.value = "";
+    phone.value = "";
+    para_nom.classList.add('hidden');
+    para_role.classList.add('hidden');
+    para_url.classList.add('hidden');
+    para_email.classList.add('hidden');
+    para_phone.classList.add('hidden');
 })
 btn_enregistrer.addEventListener('click', () => {
     verification_presence_format();
